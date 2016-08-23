1622cb42566a6fc208be95cba15abde82ee7f9eb
geniuscodec <jacwam@gmail.com>
2016-08-23 22:58:40 +0300

    My first commit

---
+++ Pruden.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class pruden{
+   public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+      System.out.println("Welcome to Prudence Maternal Hospital");
+
+      System.out.println("Input an option, either doctors or mothers: ");
+      String option = input.next();
+
+      System.out.println("You are in the doctor's database, please choose doctor's name");
+      System.out .println("Do  you want to edit the information?");
+      String edit = input.nextLine();
+     
+     }
+}
